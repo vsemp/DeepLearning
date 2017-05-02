917dd56343a9ef484368841db65b2f60c513b44b
Vladimir Pchelin <vsemp@bu.edu>
2017-05-02 09:42:16 -0400

    My changes to train the first approach

--- FirstApproach/cifar10.py
+++ FirstApproach/cifar10.py
@@ -50,7 +50,7 @@ FLAGS = tf.app.flags.FLAGS
 # Basic model parameters.
 tf.app.flags.DEFINE_integer('batch_size', 128,
                             """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data',
+tf.app.flags.DEFINE_string('data_dir', './cifar10_data',
                            """Path to the CIFAR-10 data directory.""")
 tf.app.flags.DEFINE_boolean('use_fp16', False,
                             """Train the model using fp16.""")
@@ -60,6 +60,12 @@ IMAGE_SIZE = cifar10_input.IMAGE_SIZE
 NUM_CLASSES = cifar10_input.NUM_CLASSES
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN
 NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
+
+# Constants for my project (c) Vladimir
+GRAD_FLOW = True
+
+def reduction(var):
+  return var + tf.stop_gradient( tf.sign( tf.round(var) ) - var )
 
 
 # Constants describing the training process.
@@ -112,7 +118,7 @@ def _variable_on_cpu(name, shape, initializer):
   return var
 
 
-def _variable_with_weight_decay(name, shape, stddev, wd):
+def _variable_with_weight_decay(name, shape, stddev, wd1=None, wd2=None):
   """Helper to create an initialized Variable with weight decay.
 
   Note that the Variable is initialized with a truncated normal distribution.
@@ -133,8 +139,12 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
       name,
       shape,
       tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
-  if wd is not None:
-    weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+  if wd1 is not None:
+    l1 = tf.reduce_mean(  tf.minimum( tf.abs( tf.abs(var) - 1 ), tf.abs(var) ) )
+    weight_decay_l1 = tf.multiply(l1, wd1, name='weight_loss_l1')
+    tf.add_to_collection('l1losses', weight_decay_l1)
+  if wd2 is not None:
+    weight_decay = tf.multiply(tf.nn.l2_loss( tf.maximum(tf.abs(var) - 1, 0) ), wd2, name='weight_loss_l2')
     tf.add_to_collection('losses', weight_decay)
   return var
 
@@ -184,8 +194,7 @@ def inputs(eval_data):
     labels = tf.cast(labels, tf.float16)
   return images, labels
 
-
-def inference(images):
+def inference(images, penalty=None):
   """Build the CIFAR-10 model.
 
   Args:
@@ -201,10 +210,11 @@ def inference(images):
   #
   # conv1
   with tf.variable_scope('conv1') as scope:
-    kernel = _variable_with_weight_decay('weights',
+    coef = _variable_on_cpu('coef', [], tf.constant_initializer(0.04))
+    kernel = coef * reduction(_variable_with_weight_decay('weights',
                                          shape=[5, 5, 3, 64],
-                                         stddev=5e-2,
-                                         wd=0.0)
+                                         stddev=3, wd2=0.001))
+    #images = tf.layers.batch_normalization(images)
     conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
     biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
     pre_activation = tf.nn.bias_add(conv, biases)
@@ -214,46 +224,50 @@ def inference(images):
   # pool1
   pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
                          padding='SAME', name='pool1')
-  # norm1
-  norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
-                    name='norm1')
 
   # conv2
   with tf.variable_scope('conv2') as scope:
-    kernel = _variable_with_weight_decay('weights',
+    coef = _variable_on_cpu('coef', [], tf.constant_initializer(0.04))
+    kernel = coef * reduction(_variable_with_weight_decay('weights',
                                          shape=[5, 5, 64, 64],
-                                         stddev=5e-2,
-                                         wd=0.0)
-    conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
+                                         stddev=3, wd2=0.001))
+    pool1 = tf.layers.batch_normalization(pool1)
+    conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
     biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
     pre_activation = tf.nn.bias_add(conv, biases)
     conv2 = tf.nn.relu(pre_activation, name=scope.name)
     _activation_summary(conv2)
 
-  # norm2
-  norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
-                    name='norm2')
   # pool2
-  pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
+  pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1],
                          strides=[1, 2, 2, 1], padding='SAME', name='pool2')
 
   # local3
   with tf.variable_scope('local3') as scope:
+    pool2 = tf.layers.batch_normalization(pool2)
     # Move everything into depth so we can perform a single matrix multiply.
     reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
     dim = reshape.get_shape()[1].value
-    weights = _variable_with_weight_decay('weights', shape=[dim, 384],
-                                          stddev=0.04, wd=0.004)
+
+    coef = _variable_on_cpu('coef', [], tf.constant_initializer(0.04))
+    weights = coef * reduction(_variable_with_weight_decay('weights', shape=[dim, 384],
+                                          stddev=3.04, wd2=0.001))
+
     biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
-    local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+    local3 = tf.nn.relu(tf.matmul(reshape, (weights) ) + biases, name=scope.name)
     _activation_summary(local3)
 
   # local4
   with tf.variable_scope('local4') as scope:
-    weights = _variable_with_weight_decay('weights', shape=[384, 192],
-                                          stddev=0.04, wd=0.004)
+    local3 = tf.layers.batch_normalization(local3)
+
+    coef = _variable_on_cpu('coef', [], tf.constant_initializer(0.04))
+    weights = coef *  reduction(_variable_with_weight_decay('weights', shape=[384, 192],
+                                          stddev=3.04, wd2=0.001))
+
     biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
-    local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+    
+    local4 = tf.nn.relu(tf.matmul(local3, (weights) ) + biases, name=scope.name)
     _activation_summary(local4)
 
   # linear layer(WX + b),
@@ -261,8 +275,11 @@ def inference(images):
   # tf.nn.sparse_softmax_cross_entropy_with_logits accepts the unscaled logits
   # and performs the softmax internally for efficiency.
   with tf.variable_scope('softmax_linear') as scope:
-    weights = _variable_with_weight_decay('weights', [192, NUM_CLASSES],
-                                          stddev=1/192.0, wd=0.0)
+
+    coef = _variable_on_cpu('coef', [], tf.constant_initializer(0.04))
+    weights = coef * reduction(_variable_with_weight_decay('weights', [192, NUM_CLASSES],
+                                          stddev=3.04, wd2=0.001))
+
     biases = _variable_on_cpu('biases', [NUM_CLASSES],
                               tf.constant_initializer(0.0))
     softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
@@ -290,9 +307,16 @@ def loss(logits, labels):
   cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
   tf.add_to_collection('losses', cross_entropy_mean)
 
+  # Accuracy
+
+  top_k_op = tf.cast(tf.nn.in_top_k(tf.cast(logits,
+  tf.float32), labels, 1),tf.float32)
+  accuracy = tf.reduce_mean(top_k_op, name='accuracy') 
+  tf.add_to_collection('evaluation', accuracy)
+
   # The total loss is defined as the cross entropy loss plus all of the weight
   # decay terms (L2 loss).
-  return tf.add_n(tf.get_collection('losses'), name='total_loss')
+  return tf.add_n(tf.get_collection('losses'), name='total_loss'), accuracy
 
 
 def _add_loss_summaries(total_loss):
@@ -309,11 +333,12 @@ def _add_loss_summaries(total_loss):
   # Compute the moving average of all individual losses and the total loss.
   loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
   losses = tf.get_collection('losses')
-  loss_averages_op = loss_averages.apply(losses + [total_loss])
+  evaluation = tf.get_collection('evaluation')
+  loss_averages_op = loss_averages.apply(losses + [total_loss] + evaluation)
 
   # Attach a scalar summary to all individual losses and the total loss; do the
   # same for the averaged version of the losses.
-  for l in losses + [total_loss]:
+  for l in losses + [total_loss] + evaluation:
     # Name each loss as '(raw)' and name the moving average version of the loss
     # as the original loss name.
     tf.summary.scalar(l.op.name + ' (raw)', l)
@@ -352,8 +377,8 @@ def train(total_loss, global_step):
 
   # Compute gradients.
   with tf.control_dependencies([loss_averages_op]):
-    opt = tf.train.GradientDescentOptimizer(lr)
-    grads = opt.compute_gradients(total_loss)
+    opt = tf.train.AdamOptimizer()
+    grads = opt.compute_gradients(total_loss) # + tf.add_n(tf.get_collection('l1losses')) * CUSTOM_LOSS * tf.maximum(global_step - CUSTOM_START_STEP, 0) )
 
   # Apply gradients.
   apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
--- FirstApproach/cifar10_train.py
+++ FirstApproach/cifar10_train.py
@@ -37,29 +37,30 @@ from __future__ import division
 from __future__ import print_function
 
 from datetime import datetime
+import os.path
 import time
 
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
 import cifar10
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
+tf.app.flags.DEFINE_string('train_dir', './cifar10_train',
                            """Directory where to write event logs """
                            """and checkpoint.""")
-tf.app.flags.DEFINE_integer('max_steps', 1000000,
+tf.app.flags.DEFINE_integer('max_steps', 20000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
-tf.app.flags.DEFINE_integer('log_frequency', 10,
-                            """How often to log results to the console.""")
 
 
-def train():
+def train(save=True, load=False):
   """Train CIFAR-10 for a number of steps."""
   with tf.Graph().as_default():
-    global_step = tf.contrib.framework.get_or_create_global_step()
+    global_step = tf.Variable(0.0, trainable=False)
 
     # Get images and labels for CIFAR-10.
     images, labels = cifar10.distorted_inputs()
@@ -69,47 +70,60 @@ def train():
     logits = cifar10.inference(images)
 
     # Calculate loss.
-    loss = cifar10.loss(logits, labels)
+    loss, accuracy = cifar10.loss(logits, labels)
 
     # Build a Graph that trains the model with one batch of examples and
     # updates the model parameters.
     train_op = cifar10.train(loss, global_step)
 
-    class _LoggerHook(tf.train.SessionRunHook):
-      """Logs loss and runtime."""
+    # Create a saver.
+    saver = tf.train.Saver(tf.all_variables())
 
-      def begin(self):
-        self._step = -1
-        self._start_time = time.time()
+    # Build the summary operation based on the TF collection of Summaries.
+    summary_op = tf.summary.merge_all()
 
-      def before_run(self, run_context):
-        self._step += 1
-        return tf.train.SessionRunArgs(loss)  # Asks for loss value.
+    # Build an initialization operation to run below.
+    init = tf.global_variables_initializer()
 
-      def after_run(self, run_context, run_values):
-        if self._step % FLAGS.log_frequency == 0:
-          current_time = time.time()
-          duration = current_time - self._start_time
-          self._start_time = current_time
+    # Start running operations on the Graph.
+    sess = tf.Session(config=tf.ConfigProto(
+        log_device_placement=FLAGS.log_device_placement))
+    sess.run(init)
 
-          loss_value = run_values.results
-          examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
-          sec_per_batch = float(duration / FLAGS.log_frequency)
+    # Start the queue runners.
+    tf.train.start_queue_runners(sess=sess)
 
-          format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                        'sec/batch)')
-          print (format_str % (datetime.now(), self._step, loss_value,
-                               examples_per_sec, sec_per_batch))
+    summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
 
-    with tf.train.MonitoredTrainingSession(
-        checkpoint_dir=FLAGS.train_dir,
-        hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
-               tf.train.NanTensorHook(loss),
-               _LoggerHook()],
-        config=tf.ConfigProto(
-            log_device_placement=FLAGS.log_device_placement)) as mon_sess:
-      while not mon_sess.should_stop():
-        mon_sess.run(train_op)
+    # Load
+    if load:
+      saver.restore(sess, checkpoint_path)
+
+    for step in xrange(FLAGS.max_steps):
+      start_time = time.time()
+      _, loss_value, accuracy_value = sess.run([train_op, loss, accuracy])
+      duration = time.time() - start_time
+
+      # assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+
+      if step % 10 == 0:
+        num_examples_per_step = FLAGS.batch_size
+        examples_per_sec = num_examples_per_step / duration
+        sec_per_batch = float(duration)
+
+        format_str = ('%s: step %d, loss = %.2f accuracy = %.2f (%.1f examples/sec; %.3f '
+                      'sec/batch)')
+        print (format_str % (datetime.now(), sess.run(global_step) - 1, loss_value, accuracy_value,
+                             examples_per_sec, sec_per_batch))
+
+      if step % 100 == 0:
+        summary_str = sess.run(summary_op)
+        summary_writer.add_summary(summary_str, step)
+
+      # Save the model checkpoint periodically.
+      if save and (step % 1000 == 0 or (step + 1) == FLAGS.max_steps):
+        checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+        saver.save(sess, checkpoint_path)
 
 
 def main(argv=None):  # pylint: disable=unused-argument
